8e630ce58bdbc3b1241c6f26c429a1b6c5be1bc3
ningque <zxh_special@163.com>
2019-10-11 20:29:24 +0800

    搜索旋转排序数组

---
+++ src/main/java/com/zxh/leetcode/byteDance/Search.java
@@ -0,0 +1,50 @@
+package com.zxh.leetcode.byteDance;
+
+/**
+ * 题目描述：
+ *  假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ *
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ *
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ *
+ * 你可以假设数组中不存在重复的元素。
+ *
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * 输出: 4
+ * 示例 2:
+ *
+ * 输入: nums = [4,5,6,7,0,1,2], target = 3
+ * 输出: -1
+ *
+ * 解题思路：
+ *  采用二分法解决，但是需要考虑到多种情况！
+ */
+public class Search {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0, right = len-1;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < nums[right]){
+                if(nums[mid] < target && target <= nums[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }else{
+                if(nums[left] <= target && target < nums[mid])
+                    right = mid -1;
+                else
+                    left = mid +1;
+            }
+        }
+        return -1;
+    }
+}
