99f1857a2a5d2c3587f07b6eb82cce1e3835366a
ningque <zxh_special@163.com>
2019-10-17 08:51:09 +0800

    两个有序链表合并 反转链表

---
+++ src/main/java/com/zxh/leetcode/byteDance/MergeTwoLists.java
@@ -0,0 +1,36 @@
+package com.zxh.leetcode.byteDance;
+
+import com.zxh.leetcode.tencent.ListNode;
+
+/**
+ * 题目描述：
+ *  将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
+public class MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        ListNode ans = new ListNode(0);
+        ListNode temp = ans;
+        while(l1 != null || l2 != null){
+            if(l1.val <= l2.val){
+                temp.next = l1;
+                temp = temp.next;
+                l1 = l1.next;
+            }else{
+                temp.next = l2;
+                temp = temp.next;
+                l2 = l2.next;
+            }
+        }
+        if(l1 != null){
+            temp.next = l1;
+        }else{
+            temp.next = l2;
+        }
+        return ans.next;
+    }
+}
--- src/main/java/com/zxh/leetcode/tencent/ListNode.java
+++ src/main/java/com/zxh/leetcode/tencent/ListNode.java
@@ -2,7 +2,7 @@ package com.zxh.leetcode.tencent;
 
 
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
 }
